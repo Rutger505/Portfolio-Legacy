86824c88b5dde91ea08da9c432990e3a55af3b79
Rutger <1201954@student.roc-nijmegen.nl>
2023-02-01 22:45:19 +0100

    fix try 8 it was easy all along(i think)

--- script/pageline.js
+++ script/pageline.js
@@ -5,8 +5,7 @@ const page = path.split("/").pop();
 // set line color
 const line = document.getElementsByClassName("page-button-line");
 
-if (page == "index.html" || page == "") {
-  console.log("i am here lol");
+if (page == "index.html") {
   line[0].classList.add("line-color");
 } else if (page == "my-work.html") {
   line[1].classList.add("line-color");
@@ -14,13 +13,14 @@ if (page == "index.html" || page == "") {
   line[2].classList.add("line-color");
 } else if (page == "contact.html") {
   line[3].classList.add("line-color");
+} else {
+  line[0].classList.add("line-color");
 }
 
 // set button in dropdown color
 const buttonDropdown = document.getElementsByClassName("page-button-dropdown");
 
-if (page == "index.html" || page == "") {
-  console.log("i am here lol");
+if (page == "index.html") {
   buttonDropdown[0].style.color = "#0066ff";
 } else if (page == "my-work.html") {
   buttonDropdown[1].style.color = "#0066ff";
@@ -28,4 +28,6 @@ if (page == "index.html" || page == "") {
   buttonDropdown[2].style.color = "#0066ff";
 } else if (page == "contact.html") {
   buttonDropdown[3].style.color = "#0066ff";
+} else {
+  buttonDropdown[0].classList.add("line-color");
 }
